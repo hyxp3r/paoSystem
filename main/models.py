afcd4026beba621d4794df9955cd623637be1cb8
hyxp3r <erma2001@mail.ru>
2022-10-19 01:37:44 +0700

    создана модель должности

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+ 
+ 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,  on_delete = models.CASCADE)
+    post = models.CharField(max_length = 200, verbose_name = "Должность" )
+
+    def __unicode__(self):
+        return self.post
