c86ec6fcd2133639cf93620a344d71e975a12cd1
Ermakov <e.n.ermakov@nsuem.ru>
2022-10-19 16:09:11 +0700

    add url to logout

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.main, name = "main"),
     path("login", views.userAuth, name = "login"),
     path("home", views.home, name = "home" ),
+    path("logout", views.user_logout, name = "logout")
 ] 
