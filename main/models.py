3f09c201df2c13d35b87a41c04d037917eac10d1
Ermakov <e.n.ermakov@nsuem.ru>
2022-10-19 16:08:30 +0700

    Change model attributes

--- main/models.py
+++ main/models.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import User
  
 class UserProfile(models.Model):
     user = models.OneToOneField(User,  on_delete = models.CASCADE)
-    post = models.CharField(max_length = 200, verbose_name = "Должность" )
+    post = models.CharField(max_length = 200, verbose_name = "Должность" , null = True)
+    department = models.CharField(max_length = 30, verbose_name = "Отдел", null = True)
 
     def __unicode__(self):
         return self.post
