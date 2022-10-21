cb9c5bb894463868836f33904067425e718f711d
Ermakov <e.n.ermakov@nsuem.ru>
2022-10-21 12:18:38 +0700

    add new app pao

---
+++ pao/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required(login_url="login")
+def paoMain(request):
+   return render(request, "pao/trackerIssues.html")
