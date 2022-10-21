0ec56cb5ff84272bc21acbe9fbf6112ba630f3d4
hyxp3r <erma2001@mail.ru>
2022-10-21 22:50:03 +0700

    смена pao ulr

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("login", views.userAuth, name = "login"),
     path("home", views.home, name = "home" ),
     path("logout", views.user_logout, name = "logout"),
-    path("pao", include("pao.urls")),
+    path("pao/", include("pao.urls")),
 ] 
