b7e2bb9e7844e20a67d5727af7862c1709d0be1e
hyxp3r <erma2001@mail.ru>
2022-10-23 20:25:06 +0700

    Добавлена обработка формы

--- pao/views.py
+++ pao/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .forms import Proccess1CFileForm
+from .files import files1C
 
+import pandas as pd
 # Create your views here.
 @login_required(login_url="login")
 def paoMain(request):
@@ -12,8 +16,26 @@ def paoMain(request):
 @login_required(login_url="login")
 def file(request):
 
-   if request.method == "POST":
-      username = request.POST.get("flexRadioDefault").value()
-      print(username)
+   form = Proccess1CFileForm()
 
-   return render(request,"pao/file.html")
+   if request.method == "POST":
+
+      form = Proccess1CFileForm(request.POST, request.FILES)
+      if form.is_valid():
+         
+         type = form.cleaned_data["types"]
+         file = form.cleaned_data["file"]
+         response = files1C().getInfo(file, type)
+         
+         if response:
+            return response
+         else:
+            messages.info(request, "Запрос отклонен. Проверьте файл на соответствие шаблону 1C.")
+      else:
+            messages.info(request, "Запрос отклонен. Загрузите файл формата .xlsx")
+         
+            
+         
+
+
+   return render(request,"pao/file.html", {"form":form})
