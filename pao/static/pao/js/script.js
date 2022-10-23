855be2718feb7c1e0715c4674f6beb9a626b84f4
hyxp3r <erma2001@mail.ru>
2022-10-23 23:44:59 +0700

    Добавлены js скрипты

---
+++ pao/static/pao/js/script.js
@@ -0,0 +1,14 @@
+window.onload = init;
+
+function deleteWarning() {
+    
+    const elem = document.getElementById("formAlert");
+    elem.remove();
+}
+
+function init() {
+
+    const radiobtn = document.getElementById("id_types_0");
+    radiobtn.checked = true;
+}
+
