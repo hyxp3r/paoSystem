cf2570c66979c23bcb463c98765b4444596ab3ac
Ermakov <e.n.ermakov@nsuem.ru>
2022-10-25 16:53:41 +0700

    add new url for tracker

--- pao/urls.py
+++ pao/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.paoMain, name = "paoMain"),
-    path("files", views.file, name = "file" )
+    path("files", views.file, name = "file" ),
+    path("tracker", views.tracker, name = "tracker" ),
     
     
 ] 
