7edd931dc50b2b16b47e792a3f78725be9572fe7
Ermakov <e.n.ermakov@nsuem.ru>
2022-10-27 17:23:25 +0700

    new urls name

--- pao/urls.py
+++ pao/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
     path("", views.paoMain, name = "paoMain"),
-    path("files", views.file, name = "file" ),
-    path("tracker", views.tracker, name = "tracker" ),
+    path("files/1C", views.file, name = "file1C" ),
+    path("tracker", views.tracker, name = "trackerDash" ),
     
     
 ] 
