e4cdc546ba7dd6e34933fa373a38c510ad6b58df
Ermakov <e.n.ermakov@nsuem.ru>
2022-10-29 16:17:05 +0700

    add url for api

--- paoSystem/urls.py
+++ paoSystem/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from pao.views import TrackerAPI
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path('__debug__/', include('debug_toolbar.urls')),
+    path("api/v1/trackerreport/", TrackerAPI.as_view())
 ]
