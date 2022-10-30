91c71d0f40cc5c352a3fb9fbd8c6c093dc1b7578
hyxp3r <erma2001@mail.ru>
2022-10-30 21:57:01 +0700

    Сериалазйер настроен на обработку данных с модели

--- pao/serializer.py
+++ pao/serializer.py
@@ -1,33 +1,13 @@
 from dataclasses import field
 from rest_framework import serializers
+from .models import Tracker
 
 
+class TrackerSerializer(serializers.ModelSerializer):
 
+    class Meta:
 
-class TrackerSerializer(serializers.Serializer):
+        model = Tracker
+        fields = "__all__"
+        
 
-    createdTime = serializers.DateTimeField()
-    nobody = serializers.IntegerField(default = 0)
-    soglDecan = serializers.IntegerField( default = 0)
-    decanFinans = serializers.IntegerField(default = 0)
-    bezRectorVisa = serializers.IntegerField(default = 0)
-    paoCan = serializers.IntegerField(default = 0)
-
-    naSoglDecan = serializers.IntegerField(default = 0)
-    fbp = serializers.IntegerField(default = 0)
-    fgs = serializers.IntegerField(default = 0)
-    fcep = serializers.IntegerField(default = 0)
-    uf = serializers.IntegerField(default = 0)
-
-    remake = serializers.IntegerField(default = 0)
-    remakeDecan = serializers.IntegerField(default = 0)
-    remakeAup = serializers.IntegerField(default = 0)
-    remakeRector = serializers.IntegerField(default = 0)
-
-    naSoglAdv = serializers.IntegerField(default = 0)
-    naSoglProdject = serializers.IntegerField(default = 0)
-    naSoglYung = serializers.IntegerField(default = 0)
-    naSoglLearn = serializers.IntegerField(default = 0)
-    naSoglHr = serializers.IntegerField( default = 0)
-    naSoglScience = serializers.IntegerField(default = 0)
-    naSoglredact = serializers.IntegerField(default = 0)
